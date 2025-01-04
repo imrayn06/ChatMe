1943f9ca6dceeec62fd3f105c0c27009ff0ead23
Rayn <duttarayan3@gmail.com>
2025-01-05 03:13:05 +0530

    Send message api at backend

--- backend/server.jsx
+++ backend/server.jsx
@@ -5,14 +5,18 @@ const cors = require("cors");
 const connectDB = require("./config/db.jsx");
 const colors = require("colors");
 const userRoutes = require("./routes/userRoutes.jsx");
-const chatRoutes = require("./routes/chatRoutes.jsx")
-const { notFound, errorHandler } = require("./middleware/errorMiddleware.jsx").default;
+const chatRoutes = require("./routes/chatRoutes.jsx");
+const messageRoutes = require("./routes/messageRoutes.jsx");
+const { notFound, errorHandler } =
+  require("./middleware/errorMiddleware.jsx").default;
 const app = express();
 dotenv.config();
 connectDB();
-app.use(cors({
-  origin: "http://localhost:5174", 
-}));
+app.use(
+  cors({
+    origin: "http://localhost:5174",
+  })
+);
 app.use(express.json()); //to accept json data
 
 app.get("/", (req, res) => {
@@ -21,7 +25,9 @@ app.get("/", (req, res) => {
 
 app.use("/api/user", userRoutes);
 
-app.use("/api/chat",chatRoutes)
+app.use("/api/chat", chatRoutes);
+
+app.use("/api/message", messageRoutes);
 
 app.use(notFound);
 app.use(errorHandler);
